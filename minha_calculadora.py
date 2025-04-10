52a26ae4c28dc0a75abfbb507787d1c060ab3fa7
DevKarolinyBarreto <karolinybarreto65@gmail.com>
2025-04-09 21:48:13 -0300

    Código completo

---
+++ minha_calculadora.py
@@ -0,0 +1,18 @@
+def soma(a,b):
+    soma = a + b
+    return soma
+
+def subtracao (a,b):
+    sub = a - b
+    return sub
+
+def multiplicacao (a,b):
+    multi = a * b
+    return multi 
+
+def divisao(a,b):
+    if b == 0:
+        print("Erro")
+    else:
+         return a / b
+        
---
+++ teste_calculadora.py
@@ -0,0 +1,10 @@
+import minha_calculadora as mc
+
+#soma
+print(mc.soma(10,5))
+#subtracao
+print(mc.subtracao(7,20))
+#multiplicacao
+print(mc.multiplicacao(4,6))
+#divisao
+print(mc.divisao(16,4))
